e4461ae835f62b0749d46afadd32b02426f98a70
vishalgowda26 <vishalygowda@gmail.com>
2025-02-11 12:09:30 +0530

    Implemented fallback path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: PagenotfoundComponent },
 ];
 
 @NgModule({

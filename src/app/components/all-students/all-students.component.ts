b49274bc6166284691061cfa84524512b5fcf873
vishalgowda26 <vishalygowda@gmail.com>
2025-02-13 15:32:30 +0530

    Table implemented with API integration

--- src/app/components/all-students/all-students.component.ts
+++ src/app/components/all-students/all-students.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-all-students',
   templateUrl: './all-students.component.html',
-  styleUrls: ['./all-students.component.scss']
+  styleUrls: ['./all-students.component.scss'],
 })
-export class AllStudentsComponent {
+export class AllStudentsComponent implements OnInit {
+  students: any = [];
 
+  constructor(private _studentService: StudentService) {}
+  
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents() {
+    this._studentService.getStudents().subscribe((data: any) => {
+      this.students = data;
+      console.log('All-Students', this.students);
+    });
+  }
 }

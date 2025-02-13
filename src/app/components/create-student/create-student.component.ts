edac52a212b5361a6f9e8fd10e37929ce2b5ed2f
vishalgowda26 <vishalygowda@gmail.com>
2025-02-13 20:43:52 +0530

    Fixed sourceForm issue in createstudent component

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators,} from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { StudentService } from 'src/app/services/student.service';
 
 @Component({
@@ -11,7 +18,10 @@ export class CreateStudentComponent {
   studentForm: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required]),
     gender: new FormControl('', [Validators.required]),
-    mobile: new FormControl('', [ Validators.required, Validators.min(1000000000), Validators.max(9999999999),
+    mobile: new FormControl('', [
+      Validators.required,
+      Validators.min(1000000000),
+      Validators.max(9999999999),
     ]),
     email: new FormControl('', [Validators.required]),
     batch: new FormControl('', [Validators.required]),
@@ -20,7 +30,11 @@ export class CreateStudentComponent {
       mandal: new FormControl(),
       district: new FormControl('', [Validators.required]),
       state: new FormControl(),
-      pincode: new FormControl('', [ Validators.required, Validators.min(100000), Validators.max(999999), noLeadingZero,
+      pincode: new FormControl('', [
+        Validators.required,
+        Validators.min(100000),
+        Validators.max(999999),
+        noLeadingZero,
       ]),
     }),
     education: new FormArray([]),
@@ -70,36 +84,19 @@ export class CreateStudentComponent {
 
   constructor(private _studentService: StudentService) {
     this.studentForm.get('sourceType')?.valueChanges.subscribe((data: any) => {
-      // Reset form by removing dynamic controls
+      // Reset dynamic fields
       this.studentForm.removeControl('sourceForm');
-      this.studentForm.removeControl('socialmedia');
-      this.studentForm.removeControl('office');
       this.studentForm.removeControl('referralName');
 
       if (data === 'Direct') {
-        // Add sourceForm control for Direct type
+        // `sourceForm` stores only "socialmedia" or "office"
         this.studentForm.addControl('sourceForm', new FormControl());
-
-        // Listen to sourceForm changes
-        this.studentForm.get('sourceForm')?.valueChanges.subscribe((sourceData: any) => {
-            this.studentForm.removeControl('socialmedia');
-            this.studentForm.removeControl('office');
-
-            if (sourceData === 'socialmedia') {
-              this.studentForm.addControl('socialmedia', new FormControl(''));
-            } else if (sourceData === 'office') {
-              this.studentForm.addControl('office', new FormControl());
-            }
-          });
-
       } else if (data === 'Refer') {
-        // Add referralName only for Refer type
         this.studentForm.addControl('referralName', new FormControl());
       }
     });
   }
 }
-
 
 // Custom Validator Function to check if the first character is '0'
 function noLeadingZero(control: AbstractControl): ValidationErrors | null {

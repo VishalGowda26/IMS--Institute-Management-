ec8c8ed0812daffb7fec350b579b67ce25ff1781
vishalgowda26 <vishalygowda@gmail.com>
2025-02-13 19:23:59 +0530

    Added missing files

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -14,4 +14,24 @@ export class StudentService {
   createStudent(payload: Students): Observable<any> {
     return this._httpClient.post(this.baseUrl, payload);
   }
+
+  getStudents(): Observable<any> {
+    return this._httpClient.get(this.baseUrl);
+  }
+
+  getPagedStudents(limit: number, page: number): Observable<any> {
+    return this._httpClient.get(
+      this.baseUrl + '?limit=' + limit + '&page=' + page
+    );
+  }
+
+  getSortedStudents(sort: string, order: string): Observable<any> {
+    return this._httpClient.get(
+      this.baseUrl + '?sortBy=' + sort + '&order=' + order
+    );
+  }
+
+  getFilteredStudents(word: string): Observable<any> {
+    return this._httpClient.get(this.baseUrl + '?filter=' + word);
+  }
 }
